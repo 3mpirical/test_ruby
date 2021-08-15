1308b5ba68f5bb858a2156728570e8124c246a86
Nick Ristagno <33384149+3mpirical@users.noreply.github.com>
2021-08-15 12:50:04 +0000

    Added run statement to config.ru

--- main.rb
+++ main.rb
@@ -1,12 +1,14 @@
 require "sinatra"
 
-before "/" do
-  content_type :json
-  if params["token"] != "nick-test-token"
-    halt 422, { error: "Something went wrong" }.to_json
+class Main < Sinatra::Base
+  before do
+    content_type :json
+    if params["token"] != "nick-test-token"
+      halt 422, { error: "Something went wrong" }.to_json
+    end
   end
-end
 
-get "/" do
-  { data: "hello json world" }.to_json
+  get "/" do
+    { data: "hello json world" }.to_json
+  end
 end
